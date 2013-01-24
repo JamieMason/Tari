3b22ed7a4de5c53d0cf132f86b7f2ddf87135417
Jamie Mason <siunmaru@gmail.com>
2013-01-24 20:51:51 +0000

    Test config

---
+++ testacular.conf.js
@@ -0,0 +1,46 @@
+reporters = ['coverage', 'progress', 'junit'];
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  REQUIRE,
+  REQUIRE_ADAPTER,
+  { pattern: '**/src-test/lib/remote-file.txt', watched: false, included: false, served: true },
+  '**/vendor/require-2.0.6.js',
+  '**/vendor/jasmine-matchers-1.1.0.js',
+  '**/config-test.js',
+  '**/src/*.js',
+  '**/src-test/*.js'
+];
+
+proxies = {
+  '/server': 'http://localhost:9000'
+};
+
+preprocessors = {
+  '**/src/*.js': 'coverage'
+};
+
+coverageReporter = {
+  type : 'html',
+  dir : 'src-test/coverage/'
+};
+
+junitReporter = {
+  outputFile: 'src-test/output/test-results.xml'
+};
+
+// browsers = ['PhantomJS'];
+
+// run on save
+autoWatch = !false;
+singleRun = !true;
+
+// system
+port = 7357;
+runnerPort = 9100;
+captureTimeout = 5000;
+
+// stdout
+logLevel = LOG_INFO;
+colors = true;
