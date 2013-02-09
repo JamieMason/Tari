1fd84bb2eb179442a6bec2ca16d5e7c50d706c1d
Jamie Mason <siunmaru@gmail.com>
2013-02-09 23:54:31 +0000

    added toArray

--- src/collection.js
+++ src/collection.js
@@ -1,59 +1,50 @@
-define('collection', [
-    'fn',
-    'each'
-  ],
+define('collection', ['fn', 'each'], function($fn, $each) {
 
-  function(
-    $fn,
-    $each
-  ) {
+  'use strict';
 
-    'use strict';
+  var exports = {};
 
-    var curry = $fn.curry;
-
-    /**
-     * Iterates over the Object to create a single item using the memo provided
-     * @param  {Function} f
-     * @param  {Mixed} memo
-     * @param  {Object} xs
-     * @return {Mixed}
-     */
-    function fold(f, memo, xs) {
-      $each(xs, function(val, key) {
-        memo = f(memo, val, key);
-      });
-      return memo;
-    }
-
-    /**
-     * Iterate over every member of xs, calling f with the arguments: member, i, xs to create a new array of f's return values
-     * @param  {Array|Object} xs
-     * @param  {Function} f
-     * @return {Array|Object}
-     */
-    function map(xs, f) {
-      return fold(function(memo, val, key) {
-        memo[key] = f(val, key, xs);
-        return memo;
-      }, {}.toString.call(xs).slice(8, -1) === 'Array' ? [] : {}, xs);
-    }
-
-    return {
-      fold: curry(fold),
-      map: curry(map),
-
-      /**
-       * Convert an Array-like Object into a true Array
-       * @param  {Array|Object|NodeList} xs
-       * @return {Array}
-       */
-      toArray: function(xs) {
-        return fold(function(memo, val) {
-          memo.push(val);
-          return memo;
-        }, [], xs);
-      }
-    };
+  function fold(iterator, memo, collection) {
+    $each(collection, function(val, key) {
+      memo = iterator(memo, val, key);
+    });
+    return memo;
   }
-);
+
+  /**
+   * Iterates over the Object to create a single item using the memo provided
+   * @param  {Function} iterator
+   * @param  {Mixed} memo
+   * @param  {Object} collection
+   * @return {Mixed}
+   */
+  exports.fold = $fn.curry(fold);
+
+  /**
+   * Iterate over every member of collection, calling iterator with the arguments: member, i, collection to create a new array of iterator's return values
+   * @param  {Array|Object} collection
+   * @param  {Function} iterator
+   * @return {Array|Object}
+   */
+  exports.map = $fn.curry(function(collection, iterator) {
+    return fold(function(memo, val, key) {
+      memo[key] = iterator(val, key, collection);
+      return memo;
+    }, {}.toString.call(collection).slice(8, -1) === 'Array' ? [] : {}, collection);
+  });
+
+  /**
+   * Convert an Array-like Object into a true Array
+   * @param  {Array|Object|NodeList} collection
+   * @return {Array}
+   */
+  exports.toArray = function(collection) {
+    return fold(function(memo, val) {
+      memo.push(val);
+      return memo;
+    }, [], collection);
+  };
+
+  return exports;
+
+});
