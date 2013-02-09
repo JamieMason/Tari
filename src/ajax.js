38d1242ea4fe62463db7f7766a09ba6678cb3a6e
Jamie Mason <siunmaru@gmail.com>
2013-02-09 23:54:56 +0000

    remove jsDoc from private code

--- src/ajax.js
+++ src/ajax.js
@@ -1,146 +1,111 @@
-define('ajax', [
-    'fn',
-    'value',
-    'url'
-  ],
+define('ajax', ['fn', 'value', 'url'], function($fn, $value, $url) {
 
-  function(
-    $fn,
-    $value,
-    $url
-  ) {
+  'use strict';
 
-    'use strict';
+  // Object
+  var exports = {};
+  // Boolean
+  var hasXhr = !$value.isUndefined(window.XMLHttpRequest);
+  // Boolean
+  var hasAxo = !$value.isUndefined(window.ActiveXObject);
+  // Boolean
+  var hasAjax = hasXhr || hasAxo;
+  // Function
+  var newRequest;
 
-    var global = window;
-    var compose = $fn.compose;
-    var hasXhr = !$value.isUndefined(global.XMLHttpRequest);
-    var hasAxo = !$value.isUndefined(global.ActiveXObject);
-    var hasAjax = hasXhr || hasAxo;
+  // open a connection for the option's request Object
+  // (String, Object) -> Object
+  var openConnection = $fn.curry(function(kind, options) {
+    options.request.open(kind, options.url, true);
+    return options;
+  });
 
-    /**
-     * Open a connection for the option's request Object
-     * @private
-     * @param  {String} kind "GET" or "POST"
-     * @param  {Object} ops
-     * @return {Object} ops
-     */
-    var openConnection = $fn.curry(function(kind, ops) {
-      ops.request.open(kind, ops.url, true);
-      return ops;
-    });
-
-    /**
-     * @private
-     * @return {XMLHttpRequest}
-     */
-    function newXhr() {
-      return new XMLHttpRequest();
-    }
-
-    /**
-     * @private
-     * @return {ActiveXObject}
-     */
-    function newAxo() {
-      return new ActiveXObject('Microsoft.XMLHTTP');
-    }
-
-    var newRequest = hasXhr ? newXhr : hasAxo ? newAxo : $fn.noOp();
-
-    /**
-     * Throw error if the attempted request parameters are not valid
-     * @private
-     * @param  {Object} ops
-     * @return {Object} ops
-     */
-    function throwIfInvalid(ops) {
-      if (validate(ops)) {
-        return ops;
-      }
-      throw new Error('Supports AJAX:' + hasAjax);
-    }
-
-    /**
-     * Create and initialise a new User Agent request Object
-     * @private
-     * @param  {Object} ops
-     * @return {Object} ops
-     */
-    function initRequest(ops) {
-      var req = ops.request = newRequest();
-      req.onreadystatechange = function() {
-        if (req.readyState === 4) {
-          ops.success(req.responseText);
-        }
-      };
-      return ops;
-    }
-
-    /**
-     * Implement Cache-busting if opts.noCache is true
-     * @private
-     * @param  {Object} ops
-     * @return {Object} ops
-     */
-    function handleCaching(ops) {
-      if (ops.noCache === true) {
-        ops.url = $url.addParam(ops.url, 'nocache', new Date().getTime());
-      }
-      return ops;
-    }
-
-    /**
-     * Is AJAX supported in this environment?
-     * @return {Boolean}
-     */
-    function support() {
-      return hasAjax;
-    }
-
-    /**
-     * Are the callee's supplied options valid?
-     * @param  {Object} ops
-     * @return {Boolean}
-     */
-    function validate(ops) {
-      return hasAjax && $value.isObject(ops) && $value.isString(ops.url) && $value.isFunction(ops.success);
-    }
-
-    /**
-     * Send an HTTP GET Request
-     * @param  {Object} ops
-     * @param  {String} ops.url
-     * @param  {Function} ops.success
-     * @return {XMLHttpRequest|ActiveXObject} req
-     */
-    var get = compose([throwIfInvalid, initRequest, handleCaching, openConnection('GET'), function(ops) {
-      var req = ops.request;
-      req.send('');
-      return req;
-    }]);
-
-    /**
-     * Send an HTTP JSON POST Request
-     * @param  {Object} ops
-     * @param  {String} ops.url
-     * @param  {String} ops.postBody
-     * @param  {Function} ops.success
-     * @return {XMLHttpRequest|ActiveXObject} req
-     */
-    var post = compose([throwIfInvalid, initRequest, openConnection('POST'), function(ops) {
-      var req = ops.request;
-      req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-      req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-      req.send(ops.postBody || '');
-      return req;
-    }]);
-
-    return {
-      support: support,
-      validate: validate,
-      get: get,
-      post: post
-    };
+  // () -> XMLHttpRequest
+  function newXhr() {
+    return new XMLHttpRequest();
   }
-);
+
+  // () -> ActiveXObject
+  function newAxo() {
+    return new ActiveXObject('Microsoft.XMLHTTP');
+  }
+
+  // return correct request object for the environment
+  // () -> XMLHttpRequest|ActiveXObject
+  newRequest = hasXhr ? newXhr : hasAxo ? newAxo : $fn.noOp();
+
+  // is this options object valid?
+  // (Object) -> Boolean
+  function isValid(options) {
+    return hasAjax && $value.isObject(options) && $value.isString(options.url) && $value.isFunction(options.success);
+  }
+
+  // Throw error if the attempted request parameters are not valid
+  // (Object) ->
+  function throwIfInvalid(options) {
+    if(isValid(options)) {
+      return options;
+    }
+    throw new Error('Invalid AJAX Request');
+  }
+
+  // Create and initialise a new User Agent request Object
+  // (Object) -> Object
+  function initRequest(options) {
+    var req = options.request = newRequest();
+    req.onreadystatechange = function() {
+      if(req.readyState === 4) {
+        options.success(req.responseText);
+      }
+    };
+    return options;
+  }
+
+  // Implement Cache-busting if opts.noCache is true
+  // (Object) -> Object
+  function handleCaching(options) {
+    if(options.noCache === true) {
+      options.url = $url.addParam(options.url, 'nocache', new Date().getTime());
+    }
+    return options;
+  }
+
+  /**
+   * Is AJAX supported in this environment?
+   * @return {Boolean}
+   * @memberOf ajax
+   */
+  exports.isSupported = $fn.id(hasAjax);
+
+  /**
+   * Send an HTTP GET Request
+   * @param  {String} options.url
+   * @param  {Function} options.success
+   * @return {XMLHttpRequest|ActiveXObject} req
+   * @memberOf ajax
+   */
+  exports.get = $fn.compose([throwIfInvalid, initRequest, handleCaching, openConnection('GET'), function(options) {
+    var req = options.request;
+    req.send('');
+    return req;
+  }]);
+
+  /**
+   * Send an HTTP JSON POST Request
+   * @param  {String} options.url
+   * @param  {String} options.postBody
+   * @param  {Function} options.success
+   * @return {XMLHttpRequest|ActiveXObject} req
+   * @memberOf ajax
+   */
+  exports.post = $fn.compose([throwIfInvalid, initRequest, openConnection('POST'), function(options) {
+    var req = options.request;
+    req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    req.send(options.postBody || '');
+    return req;
+  }]);
+
+  return exports;
+
+});
